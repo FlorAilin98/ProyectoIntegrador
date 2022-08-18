5864647ba031e637aeed9fff35c26463e1752424
FlorAilin98 <flor.ramirez.ailin@gmail.com>
2022-08-18 20:19:39 -0300

    cambios

---
+++ ProyectoIntegrador/BackEnd/src/main/java/com/portfolio/AR/AilinRamirezApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.AR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AilinRamirezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AilinRamirezApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador/BackEnd/src/test/java/com/portfolio/AR/AilinRamirezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.AR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AilinRamirezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
